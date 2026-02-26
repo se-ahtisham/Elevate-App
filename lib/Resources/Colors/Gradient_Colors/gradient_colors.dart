3eb1f834d5daf371e5565316d10396e57c0ba26e
Muhammad Ahtisham <se.ahtisham@gmail.com>
2026-02-26 15:05:57 +0500

    Splash Screen Complete

---
+++ lib/Resources/Colors/Gradient_Colors/gradient_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ElevateGradientColors {
+
+  static const Gradient purpleToBlue = LinearGradient(
+    colors: [Color(0xFF6200EE), Color(0xFF03DAC5)],
+    begin: Alignment.topLeft,
+    end: Alignment.bottomRight,
+  );
+  
+}
