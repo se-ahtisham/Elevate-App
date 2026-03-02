164dbf4679c933ea972281ee32e59c426b4b9ed5
Muhammad Ahtisham <se.ahtisham@gmail.com>
2026-03-02 15:30:24 +0500

    Screen Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:elevate_app/Custom_Widgets/User_Widgets/user_post_new.dart';
-import 'package:elevate_app/Pages/User_Screens/user_community_screen.dart';
+import 'package:elevate_app/Pages/User_Screens/Job_Seeker_Screens/Coding_Screens/experience_coding.dart';
+import 'package:elevate_app/Pages/User_Screens/Job_Seeker_Screens/Coding_Screens/hard_coding.dart';
+import 'package:elevate_app/Pages/User_Screens/Job_Seeker_Screens/Coding_Screens/vibe_coding.dart';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+  SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.light, // Android
+    statusBarBrightness: Brightness.dark,      // iOS
+  ),
+);
   runApp(const MyApp());
 }
 
@@ -11,9 +22,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+  
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UserCommunityScreen(),
+      home: VibeCoding(),
     );
   }
 }
