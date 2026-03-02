f10edf8df1aac938fee5ef96f0af984aec5e012f
Muhammad Ahtisham <se.ahtisham@gmail.com>
2026-03-02 14:42:39 +0500

    Files Uppdate

--- lib/Pages/User_Screens/Job_Seeker_Screens/Community_Screens/user_community_screen.dart
+++ lib/Pages/User_Screens/Job_Seeker_Screens/Community_Screens/user_community_screen.dart
@@ -1,9 +1,9 @@
 import 'package:elevate_app/Custom_Widgets/Buttons/circle_icon_button.dart';
 import 'package:elevate_app/Custom_Widgets/Text/icon_text.dart';
 import 'package:elevate_app/Navigations/community_navigation/community_navigation.dart';
-import 'package:elevate_app/Pages/User_Screens/user_community_explore_screen.dart';
-import 'package:elevate_app/Pages/User_Screens/user_community_myCommunity_screen.dart';
-import 'package:elevate_app/Pages/User_Screens/user_community_myPost_screen.dart';
+import 'package:elevate_app/Pages/User_Screens/Job_Seeker_Screens/Community_Screens/user_community_explore_screen.dart';
+import 'package:elevate_app/Pages/User_Screens/Job_Seeker_Screens/Community_Screens/user_community_myCommunity_screen.dart';
+import 'package:elevate_app/Pages/User_Screens/Job_Seeker_Screens/Community_Screens/user_community_myPost_screen.dart';
 import 'package:elevate_app/Resources/Colors/Solid_Colors/solid_colors.dart';
 import 'package:flutter/material.dart';
 
