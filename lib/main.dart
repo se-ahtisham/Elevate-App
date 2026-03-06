b0da2760bb8573348fd1dabd7a72b15320981cbf
Muhammad Ahtisham <se.ahtisham@gmail.com>
2026-03-06 15:03:18 +0500

    Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 
 import 'package:elevate_app/Pages/User_Screens/Company_Screens/Company_Dashboard_Screens/company_home_screen.dart';
+import 'package:elevate_app/Pages/User_Screens/Company_Screens/Company_Posts_Screens/company_upload_job_screen.dart';
+import 'package:elevate_app/Pages/User_Screens/Job_Seeker_Screens/Job_Seeker_Profile_Screens/job_Seeker_profile_screen.dart';
+import 'package:elevate_app/Pages/User_Screens/Job_Seeker_Screens/Job_Seeker_Profile_Screens/job_Seeker_view_profile_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -21,7 +24,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: CompanyHomeScreen(),
+      home: CompanyUploadJobScreen(),
     );
   }
 }
