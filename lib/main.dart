e7d882cba3f3edbc7a26f9174ea110351fa65392
Ahmad07o <aj120262@gmail.com>
2026-04-18 19:56:20 +0500

    Admin_Update_Company_Profile compledted

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'package:elevate_app/Pages/User_Screens/Admin_Screens/Admin_Manage%20Screens/admin_update_company_profile.dart';
+import 'package:elevate_app/Pages/User_Screens/Admin_Screens/Admin_Manage%20Screens/admin_update_job_seeker.dart';
+import 'package:elevate_app/Pages/User_Screens/Company_Screens/Compnay_Profile_Screens/company_profile.dart';
 import 'package:elevate_app/Pages/user_select.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -20,9 +23,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: UserSelect(),
-      ),
+      home: Scaffold(body: AdminUpdateCompanyProfile()),
     );
   }
 }
